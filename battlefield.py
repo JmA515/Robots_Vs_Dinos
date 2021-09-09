0062b7c5203adb938777fb819a6f0bbd45c27e9c
JmA515 <janderson515@yahoo.com>
2021-09-09 14:36:27 -0700

    Added to the welcome message. Added to the battle method and both of the show opponent options methods.

--- battlefield.py
+++ battlefield.py
@@ -11,10 +11,23 @@ class Battlefield:
         self.display_welcome()
 
     def display_welcome(self):
-        print("Welcome to Robots Vs Dinosaurs!")
+        print("Welcome to Robots Vs Dinosaurs!\nLet the battle begin!")
 
     def battle(self):
-        pass
+        all_dinos_defeated = False
+        all_robos_defeated = False
+        turn_count = 1
+        while all_dinos_defeated != True and all_robos_defeated != True:
+            self.robo_turn(random.choice(self.fleet.robots))
+            all_dinos_defeated = self.show_dino_opponent_options()
+            self.dino_turn(random.choice(self.herd.dinosaurs))
+            all_robos_defeated = self.show_robo_opponent_options()
+            print(f'Turn {turn_count}')
+            turn_count += 1
+        if all_dinos_defeated == True:
+            return "Robots Win!"
+        if all_robos_defeated == True:   
+            return "Dinosaurs Win!"
 
     def dino_turn(self, dinosaur):
         dinosaur.attack(random.choice(self.fleet.robots))
@@ -23,10 +36,18 @@ class Battlefield:
         robot.attack(random.choice(self.herd.dinosaurs))
 
     def show_dino_opponent_options(self):
-        pass
+        print(f'{self.herd.dinosaurs[0].name} has {self.herd.dinosaurs[0].health} health')
+        print(f'{self.herd.dinosaurs[1].name} has {self.herd.dinosaurs[1].health} health')
+        print(f'{self.herd.dinosaurs[2].name} has {self.herd.dinosaurs[2].health} health')
+        if self.herd.dinosaurs[0].health <= 0 and self.herd.dinosaurs[1].health <= 0 and self.herd.dinosaurs[2].health <= 0:
+            return True
 
     def show_robo_opponent_options(self):
-        pass
+        print(f'{self.fleet.robots[0].name} has {self.fleet.robots[0].health} health')
+        print(f'{self.fleet.robots[1].name} has {self.fleet.robots[1].health} health')
+        print(f'{self.fleet.robots[2].name} has {self.fleet.robots[2].health} health')
+        if self.fleet.robots[0].health <= 0 and self.fleet.robots[1].health <= 0 and self.fleet.robots[2].health <= 0:
+            return True
 
     def display_winner(self):
         pass
